72fe38f88d24acf470951027f966647a1f8ee7ed
Eolmer <dan4ik0408@gmail.com>
2024-05-23 10:56:23 +0600

    Example truck driving

---
+++ scripts/truck_driving.py
@@ -0,0 +1,88 @@
+"""
+
+Script to capture and display part of the screen
+
+"""
+# stream_local_screen
+# мини-карту врубить
+
+import time
+import os
+import sys
+from PIL import Image
+import cv2 as cv
+import numpy
+import datetime
+import hashlib
+import re
+import keyboard
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(dir_path, '..'))
+
+from autopilot.screen import stream_local_game_screen, Box
+from scripts.to_numpy import to_numpy
+
+from ets2_telemetry.truck_values import TruckValues
+from ets2_telemetry import TelemetryReader
+
+running = True
+def on_key_press(event):
+    global running
+    if event.name == 'q':
+        running = False
+
+keyboard.on_press_key('q', on_key_press)
+
+front_coord = (530, 290, 955, 610)              # Лобовое стекло
+minimax_coord = (1356, 645, 1450, 720)          # Путь на миникарте
+box = Box(0, 30, 1600, 930)
+
+DEFAULT_FPS = 10
+
+streamer = stream_local_game_screen(box=box, default_fps=DEFAULT_FPS)
+
+d = str(datetime.datetime.now()).encode('utf8')
+_train_uid = hashlib.md5(d).hexdigest()[:8]
+_img_ext = 'jpg'
+_img_path = "D:\Projects\data_autopilot"
+
+telemetry_reader = TelemetryReader()
+truck_info = TruckValues()
+
+default_fps = DEFAULT_FPS
+time_per_frame = 1.0 / default_fps
+while True:
+    start_time = time.time()
+    
+    # image_data = next(streamer)
+
+    # filename = _train_uid + '_' + re.sub(
+    # '[-:.]', '_', datetime.datetime.now().isoformat('_')[:-4]) + \
+    # '.' + _img_ext
+
+    # image = Image.fromarray(image_data, 'RGB')
+    # image = image.crop(minimax_coord)
+    #image.save(os.path.join(_img_path, filename))
+
+    # image = to_numpy(image)
+
+    # cv.imshow("Image", image)
+
+    telemetry_reader.update_telemetry(truck_info)
+    print(f'Steer (поворот руля) {truck_info.steer}', end='\t')
+    print(f'Throttle (ускорение) {truck_info.throttle}')
+    #print("------------------------------------------------\n")
+    
+    execution_time = time.time() - start_time
+    #print('FPS {}'.format(1 / (time.time() - start_time)))
+
+    if execution_time > time_per_frame:
+        #time.sleep(execution_time - time_per_frame)
+        pass
+    else:
+        time.sleep(time_per_frame - execution_time)
+    
+    # if cv.waitKey(1) == ord('q'):
+    #     cv.destroyAllWindows()
+    #     break
